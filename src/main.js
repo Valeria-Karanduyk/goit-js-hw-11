144b33ee28e9e1a8735160dc24fd64711bf7782c
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-09-18 20:20:58 +0300

    markip izitoast

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-key: 46020847 - b0dc78394505c5145868b7f5c;
+const bodySelect = document.querySelector('body');
+const firstHtml = `<div class="container">
+    <form class="formFetchEl">
+		<input type="text" class="search-input" name="search" />
+		<button class="btnEl">Search</button>
+    </form>
+    <span class="loader">Loading</span>
+	  <ul class="galleryEl"></ul>
+</div>`;
+
+bodySelect.insertAdjacentHTML('afterbegin', firstHtml);
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
